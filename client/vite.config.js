51840634ca54c5b697772ed6830b3ab5e5d5614c
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 06:53:54 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes it accessible from outside the container
+    port: process.env.PORT || 10000 // Uses the port Render provides
+  }
 })
