17b4eda6e10e5e2c792ffe3e083f239eb2ff5c2f
Realm Maid <203048206+RealmMaid@users.noreply.github.com>
2025-06-11 05:59:35 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <CartProvider>
               <WebSocketProvider>
               <App />
-              <WebSocketProvider>
+              </WebSocketProvider>
             </CartProvider>
         </AuthProvider>
       </BrowserRouter>
